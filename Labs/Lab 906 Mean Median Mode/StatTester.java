7ba7d432f79df90da024f34e1f1b1573077c681d
793724 <42591733+793724@users.noreply.github.com>
2018-09-11 18:47:22 -0700

    Fixed lab 906

--- Labs/Lab 906 Mean Median Mode/StatTester.java
+++ Labs/Lab 906 Mean Median Mode/StatTester.java
@@ -5,11 +5,7 @@
  * 
  * @author (Emma Chiu) 
  * @version (906)
- * 
- * TO FIX:
- * Mean returns a double but the division itself is integer, so it's always .0
- * Only returns one of multiple modes
- * 
+ *
  */
 
 // imports go here
@@ -18,10 +14,12 @@ import java.util.Arrays;
 public class StatTester{
     //  Instance variables
     private int[] nums;
+    private int[] mode;
     
     // Constructor
     public StatTester(){
         nums = new int[100];
+        mode = new int[11];
     }
     
     
@@ -29,6 +27,10 @@ public class StatTester{
     public void loadArray(){
         for(int i = 0; i < nums.length; i++){
               nums[i] = (int)(Math.random()*10+1);
+        }
+        
+        for(int z = 0; z < mode.length; z++){
+              mode[z] = 0;
         }
     }
     
@@ -66,34 +68,61 @@ public class StatTester{
         for(int i = 0; i < nums.length; i++){
             sum = sum + nums[i];
         }
-        return sum/nums.length;
+        double mean = ((double) sum / nums.length);
+        return mean;
     }
     
     public double getMedian(){
         if(nums.length % 2 == 0){
-            return nums[((nums.length/2)+(nums.length/2-1))/2];
+            double median = ((double) (nums[nums.length/2]+nums[nums.length/2-1])/2);
+            return median;
         } else {
-            return nums[(nums.length-1)/2];
+            double median = ((double) nums[(nums.length-1)/2]);
+            return median;
         }
     }
     
-    public int getMode(){
-        int current = nums[0];
+    public void getMode(){
+        int prior = nums[0];
         int tempCount = 0;
         int storeCount = 0;
-        int storeValue = 0;
+        int fillArray = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == current){
+            if(nums[i] == prior){
                 tempCount = tempCount + 1;
+                if(i == nums.length - 1 && tempCount >= storeCount){
+                    if(tempCount > storeCount){
+                       storeCount = tempCount;
+                       for(int z = 0; z < mode.length; z++){
+                           mode[z] = 0;
+                       }
+                       fillArray = 0;
+                    }
+                    mode[fillArray] = nums[i-1];
+                    fillArray++;
+                }
             } else {
                 if(tempCount >= storeCount){
-                    storeCount = tempCount;
-                    storeValue = nums[i-1];
+                   if(tempCount > storeCount){
+                       storeCount = tempCount;
+                       for(int z = 0; z < mode.length; z++){
+                           mode[z] = 0;
+                       }
+                       fillArray = 0;
+                   }
+                   mode[fillArray] = nums[i-1];
+                   fillArray++;
                 }
                 tempCount = 1;
             }
-            current = nums[i];
+            prior = nums[i];
         }
-        return storeValue;
+        
+        for(int i = 0; mode[i] != 0; i++){
+            System.out.print(mode[i]);
+            if(mode[i+1] != 0){
+                System.out.print(", ");
+            }
+        }
     }
 }
