fd9e0bf6f2e55ffefc7dc4550c473201b3543ed9
793724 <42591733+793724@users.noreply.github.com>
2018-10-15 10:14:30 -0700

    Project 3 Activity 2

---
+++ Projects/Project 3 - Magpie/Project 3 - Magpie/MagpieRunner2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Project 3 - Magpie
+ *
+ * @ Emma Chiu
+ * @ 1015
+ */
+
+public class MagpieRunner2 {
+    /**
+    * Create a Magpie, give it user input, and print its replies.
+    */
+    public static void main(String[] args) {
+        Magpie2 maggie = new Magpie2();
+		
+	System.out.println (maggie.getGreeting());
+	Scanner in = new Scanner (System.in);
+	String statement = in.nextLine();
+		
+	while (!statement.equals("Bye")) {
+            System.out.println (maggie.getResponse(statement));
+	    statement = in.nextLine();
+	}
+    }
+}
