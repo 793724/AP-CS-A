3de6e9b62f64d56d9b688c34175941e5bc5074cf
793724 <42591733+793724@users.noreply.github.com>
2018-10-25 10:31:27 -0700

    Started Student List

---
+++ Projects/ArrayList Project 2- Student List/StudList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+/**
+ * ArrayList Project 2- Student List
+ *
+ * @author (Emma Chiu)
+ * @version (1025)
+ */
+
+public class StudList{
+    public static void main() {
+        ArrayList<String> studList;
+        studList = new ArrayList<String>();
+    }
+}
---
+++ Projects/ArrayList Project 2- Student List/StudListRunner.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+/**
+ * ArrayList Project 2- Student List
+ *
+ * @author (Emma Chiu)
+ * @version (1025)
+ */
+public class StudListRunner{
+    /**
+    public static void main(){
+        Scanner scr = new Scanner(System.in);
+        while(true) {
+            String statement = scr.nextLine;
+        }
+            Students[i] = new Student(names[i], stuNums[i]);
+    }
+    */
+}
